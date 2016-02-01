99bc356a0c4fe4ad5068e8f200068930cda04010
Hans-Filip Elo <hansfilipelo@gmail.com>
2016-02-01 13:14:18 +0100

    Compiles and runs but shading is totally off.

--- lab1/lab1-6.c
+++ lab1/lab1-6.c
@@ -30,84 +30,6 @@ void OnTimer(int value)
 
 // Globals
 // Data would normally be read from files
-GLfloat vertices[] = {
-	-0.3f,-0.3f,-0.3f,
-	-0.3f,-0.3f, 0.3f,
-	-0.3f, 0.3f, 0.3f,
-	0.3f, 0.3f,-0.3f,
-	-0.3f,-0.3f,-0.3f,
-	-0.3f, 0.3f,-0.3f,
-	0.3f,-0.3f, 0.3f,
-	-0.3f,-0.3f,-0.3f,
-	0.3f,-0.3f,-0.3f,
-	0.3f, 0.3f,-0.3f,
-	0.3f,-0.3f,-0.3f,
-	-0.3f,-0.3f,-0.3f,
-	-0.3f,-0.3f,-0.3f,
-	-0.3f, 0.3f, 0.3f,
-	-0.3f, 0.3f,-0.3f,
-	0.3f,-0.3f, 0.3f,
-	-0.3f,-0.3f, 0.3f,
-	-0.3f,-0.3f,-0.3f,
-	-0.3f, 0.3f, 0.3f,
-	-0.3f,-0.3f, 0.3f,
-	0.3f,-0.3f, 0.3f,
-	0.3f, 0.3f, 0.3f,
-	0.3f,-0.3f,-0.3f,
-	0.3f, 0.3f,-0.3f,
-	0.3f,-0.3f,-0.3f,
-	0.3f, 0.3f, 0.3f,
-	0.3f,-0.3f, 0.3f,
-	0.3f, 0.3f, 0.3f,
-	0.3f, 0.3f,-0.3f,
-	-0.3f, 0.3f,-0.3f,
-	0.3f, 0.3f, 0.3f,
-	-0.3f, 0.3f,-0.3f,
-	-0.3f, 0.3f, 0.3f,
-	0.3f, 0.3f, 0.3f,
-	-0.3f, 0.3f, 0.3f,
-	0.3f,-0.3f, 0.3f
-};
-
-GLfloat color[] =
-{
-	0.383f,  0.771f,  0.014f,
-	0.609f,  0.115f,  0.436f,
-	0.327f,  0.483f,  0.844f,
-	0.822f,  0.369f,  0.201f,
-	0.435f,  0.602f,  0.223f,
-	0.310f,  0.747f,  0.185f,
-	0.397f,  0.770f,  0.761f,
-	0.359f,  0.436f,  0.730f,
-	0.359f,  0.383f,  0.152f,
-	0.483f,  0.396f,  0.789f,
-	0.359f,  0.861f,  0.639f,
-	0.195f,  0.348f,  0.859f,
-	0.014f,  0.184f,  0.376f,
-	0.771f,  0.328f,  0.970f,
-	0.406f,  0.615f,  0.116f,
-	0.676f,  0.977f,  0.133f,
-	0.971f,  0.372f,  0.833f,
-	0.140f,  0.616f,  0.489f,
-	0.997f,  0.313f,  0.064f,
-	0.945f,  0.719f,  0.392f,
-	0.343f,  0.021f,  0.978f,
-	0.279f,  0.317f,  0.305f,
-	0.167f,  0.620f,  0.077f,
-	0.347f,  0.857f,  0.137f,
-	0.055f,  0.953f,  0.042f,
-	0.714f,  0.305f,  0.345f,
-	0.783f,  0.290f,  0.734f,
-	0.722f,  0.645f,  0.174f,
-	0.302f,  0.455f,  0.848f,
-	0.225f,  0.387f,  0.040f,
-	0.317f,  0.713f,  0.338f,
-	0.053f,  0.959f,  0.120f,
-	0.393f,  0.621f,  0.362f,
-	0.673f,  0.211f,  0.457f,
-	0.820f,  0.883f,  0.371f,
-	0.982f,  0.099f,  0.879f
-};
 
 GLfloat translMatrix[] =
 {
@@ -193,6 +115,9 @@ void init(void)
   glVertexAttribPointer(glGetAttribLocation(program, "in_Normal"), 3, GL_FLOAT, GL_FALSE, 0, 0);
   glEnableVertexAttribArray(glGetAttribLocation(program, "in_Normal"));
 
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, bunnyIndexBufferObjID);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, m->numIndices*sizeof(GLuint), m->indexArray, GL_STATIC_DRAW);
+
 	// --------------------------------------
 
 	glUniformMatrix4fv(glGetUniformLocation(program, "translMatrix"), 1, GL_TRUE, translMatrix);
@@ -229,7 +154,7 @@ void display(void)
   glUniformMatrix4fv(glGetUniformLocation(program, "rotMatrixX"), 1, GL_TRUE, rotMatrixX);
 
   glBindVertexArray(bunnyVertexArrayObjID);	// Select VAO
-  glDrawElements(GL_TRIANGLES, m->numIndices, GL_UNSIGNED_INT, 0L);
+  glDrawElements(GL_TRIANGLES, m->numVertices, GL_UNSIGNED_INT, 0L);
 
 	printError("display");
 
@@ -240,7 +165,7 @@ int main(int argc, char *argv[])
 {
 	glutInit(&argc, argv);
 	glutInitContextVersion(3, 2);
-	glutCreateWindow ("Bunny");
+	glutCreateWindow("Bunny");
 	glutDisplayFunc(display);
 	init ();
 	OnTimer(0);
