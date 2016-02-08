d4ad173b9f604f4e7faf1dbe3435bfc4b16770d2
Hans-Filip Elo <hansfilipelo@gmail.com>
2016-02-08 14:26:14 +0100

    Added skeleton for lab2-3

---
+++ lab2/lab2-2.c
@@ -0,0 +1,200 @@
+// Questions:
+// 1) Why don't we need a buffer to upload translMatrix? Do we need buffers for everything that is not uniform (for example different colors)?
+
+
+// Lab 1-2.
+// This is the same as the first simple example in the course book,
+// but with a few error checks.
+// Remember to copy your file to a new on appropriate places during the lab so you keep old results.
+// Note that the files "lab1-1.frag", "lab1-1.vert" are required.
+
+// Should work as is on Linux and Mac. MS Windows needs GLEW or glee.
+// See separate Visual Studio version of my demos.
+#ifdef __APPLE__
+#include <OpenGL/gl3.h>
+#include "mac/MicroGlut.h"
+// Linking hint for Lightweight IDE
+// uses framework Cocoa
+#endif
+#include "GL_utilities.h"
+#include <math.h>
+#include "loadobj.h"
+#include "LoadTGA.h"
+
+#define PI 3.14159265
+
+void OnTimer(int value)
+{
+  glutPostRedisplay();
+  glutTimerFunc(20, &OnTimer, value);
+}
+
+// Globals
+// Data would normally be read from files
+
+GLfloat translMatrix[] =
+{
+  1.0f, 0.0f, 0.0f, 0.5f,
+  0.0f, 1.0f, 0.0f, 0.5f,
+  0.0f, 0.0f, 1.0f, 0.0f,
+  0.0f, 0.0f, 0.0f, 1.0f
+};
+
+GLfloat rotMatrix[] =
+{
+  1.0f, 0.0f, 0.0f, 0.0f,
+  0.0f, 1.0f, 0.0f, 0.0f,
+  0.0f, 0.0f, 1.0f, 0.0f,
+  0.0f, 0.0f, 0.0f, 1.0f
+};
+
+GLfloat rotMatrixX[] =
+{
+  1.0f, 0.0f, 0.0f, 0.0f,
+  0.0f, 1.0f, 0.0f, 0.0f,
+  0.0f, 0.0f, 1.0f, 0.0f,
+  0.0f, 0.0f, 0.0f, 1.0f
+};
+
+GLfloat phi = 0;
+GLfloat cosPhi;
+GLfloat sinPhi;
+GLfloat minSin;
+
+// Reference to shader program
+GLuint program;
+GLuint tex;
+
+Model *m;
+
+// vertex array object
+unsigned int vertexArrayObjID;
+unsigned int bunnyVertexArrayObjID;
+unsigned int bunnyTexCoordBufferObjID;
+unsigned int vertexBufferObjID;
+unsigned int colorBufferObjID;
+unsigned int bunnyVertexBufferObjID;
+unsigned int bunnyIndexBufferObjID;
+unsigned int bunnyNormalBufferObjID;
+
+void init(void)
+{
+  // vertex buffer object, used for uploading the geometry
+
+  dumpInfo(); // Dump driver info to stdout
+
+  // Load Model
+  m = LoadModelPlus("bunnyplus.obj");
+
+  // GL inits
+  glClearColor(1,1,1,0);
+  //glClearColor(0.2,0.2,0.5,0); // Color in buffer upon clear buffer
+  glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+  glEnable(GL_DEPTH_TEST);
+  printError("GL inits");
+
+  // Load and compile shader
+  program = loadShaders("lab2-2.vert", "lab2-2.frag"); // These are the programs that run on GPU
+  printError("init shader");
+
+  // Upload geometry to the GPU:
+  glGenVertexArrays(1, &bunnyVertexArrayObjID);
+  glGenBuffers(1, &bunnyVertexBufferObjID);
+  glGenBuffers(1, &bunnyIndexBufferObjID);
+  glGenBuffers(1, &bunnyNormalBufferObjID);
+  printError("glGenBuffers");
+
+  glBindVertexArray(bunnyVertexArrayObjID);
+
+  // Lab2
+  //--------------
+  glGenBuffers(1, &bunnyTexCoordBufferObjID);
+
+  if (m->texCoordArray != NULL)
+  {
+    // Load model and its texture mapping coordinates
+    glBindBuffer(GL_ARRAY_BUFFER, bunnyTexCoordBufferObjID);
+    glBufferData(GL_ARRAY_BUFFER, m->numVertices*2*sizeof(GLfloat), m->texCoordArray, GL_STATIC_DRAW);
+    glVertexAttribPointer(glGetAttribLocation(program, "inTexCoord"), 2, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(glGetAttribLocation(program, "inTexCoord"));
+  }
+
+  // Load texture into uniform in fragment shader
+  glUniform1i(glGetUniformLocation(program, "tex"), 0); // Create texture unit
+  LoadTGATextureSimple("rutor.tga", &tex); // Load texture to texture unit
+  glBindTexture(GL_TEXTURE_2D, tex); // Bind texture unit
+  glActiveTexture(GL_TEXTURE0); // Activate texture unit 0 (in case we have multiple textures, this is nessecary)
+
+  // Create a texture unit
+
+  // -----------------
+
+  // VBO for vertex data
+  glBindBuffer(GL_ARRAY_BUFFER, bunnyVertexBufferObjID);
+  glBufferData(GL_ARRAY_BUFFER, m->numVertices*3*sizeof(GLfloat), m->vertexArray, GL_STATIC_DRAW);
+  glVertexAttribPointer(glGetAttribLocation(program, "in_Position"), 3, GL_FLOAT, GL_FALSE, 0, 0);
+  glEnableVertexAttribArray(glGetAttribLocation(program, "in_Position"));
+
+  // VBO for normal data
+  glBindBuffer(GL_ARRAY_BUFFER, bunnyNormalBufferObjID);
+  glBufferData(GL_ARRAY_BUFFER, m->numVertices*3*sizeof(GLfloat), m->normalArray, GL_STATIC_DRAW);
+  glVertexAttribPointer(glGetAttribLocation(program, "in_Normal"), 3, GL_FLOAT, GL_FALSE, 0, 0);
+  glEnableVertexAttribArray(glGetAttribLocation(program, "in_Normal"));
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, bunnyIndexBufferObjID);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, m->numIndices*sizeof(GLuint), m->indexArray, GL_STATIC_DRAW);
+
+  // --------------------------------------
+
+  glUniformMatrix4fv(glGetUniformLocation(program, "translMatrix"), 1, GL_TRUE, translMatrix);
+
+  printError("init arrays");
+}
+
+
+void display(void)
+{
+  printError("pre display");
+
+  // clear the screen (using chosen color earlier)
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+  // Set rotation matrix
+  phi += PI/50;
+  cosPhi = cos(phi);
+  sinPhi = sin(phi);
+  minSin = -sinPhi;
+
+  rotMatrix[0] = cosPhi;
+  rotMatrix[5] = cosPhi;
+  rotMatrix[1] = minSin;
+  rotMatrix[4] = sinPhi;
+
+  rotMatrixX[5] = cosPhi;
+  rotMatrixX[6] = minSin;
+  rotMatrixX[9] = sinPhi;
+  rotMatrixX[10] = cosPhi;
+
+  // Send translMatrix to Vertex
+  glUniformMatrix4fv(glGetUniformLocation(program, "rotMatrix"), 1, GL_TRUE, rotMatrix);
+  glUniformMatrix4fv(glGetUniformLocation(program, "rotMatrixX"), 1, GL_TRUE, rotMatrixX);
+  glUniform1f(glGetUniformLocation(program, "time_variable"), cosPhi); // Variate shader
+
+  glBindVertexArray(bunnyVertexArrayObjID);	// Select VAO
+  glDrawElements(GL_TRIANGLES, m->numIndices, GL_UNSIGNED_INT, 0L);
+
+  printError("display");
+
+  glutSwapBuffers(); // Swap buffer so that GPU can use the buffer we uploaded to it and we can write to another
+}
+
+int main(int argc, char *argv[])
+{
+  glutInit(&argc, argv);
+  glutInitContextVersion(3, 2);
+  glutCreateWindow("Bunny");
+  glutDisplayFunc(display);
+  init ();
+  OnTimer(0);
+  glutMainLoop();
+}
---
+++ lab2/lab2-3.vert
@@ -0,0 +1,20 @@
+#version 150
+
+in vec3 in_Normal;
+in vec3 in_Position;
+in vec2 inTexCoord;
+
+uniform mat4 translMatrix;
+uniform mat4 rotMatrix;
+uniform mat4 rotMatrixX;
+
+out vec2 outTexCoord;
+out vec3 normal;
+
+void main(void)
+{
+	gl_Position = rotMatrixX * rotMatrix * vec4(in_Position, 1.0);
+
+	normal = in_Normal;
+	outTexCoord = inTexCoord;
+}
